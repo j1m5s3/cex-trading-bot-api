a763ce1f7f768f46b6c4867f5e94ccae7b31b436
j1m5s3 <james.lynch.uml@gmail.com>
2025-06-13 22:16:41 -0500

    Added azure specific Dockerfile

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
         app.run(
             debug=False,
             host="0.0.0.0",
-            port=5000
+            port=6900
         )
     else:
         if platform.system() == "Windows":
